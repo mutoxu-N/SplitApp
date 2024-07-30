cf3ab2c20e489fc6e1a8281ea4f8c543725cbf80
mutoxu=N <55544957+mutoxu-N@users.noreply.github.com>
2024-07-30 13:33:56 +0900

    RoomCreateActivityルーム作成ようの関数を作成

--- app/src/main/java/com/github/mutoxu_n/splitapp/activities/RoomCreateActivity.kt
+++ app/src/main/java/com/github/mutoxu_n/splitapp/activities/RoomCreateActivity.kt
@@ -7,10 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -34,15 +31,19 @@ class RoomCreateActivity : ComponentActivity() {
                         )
                      },
                     modifier = Modifier.fillMaxSize()
+
                 ) { innerPadding ->
                     Screen(
                         modifier = Modifier.padding(innerPadding),
-                        onSettingsChange = {}
+                        onSettingsChange = { onCreateRoom(it) }
                     )
-
                 }
             }
         }
+    }
+
+    private fun onCreateRoom(settings: Settings) {
+        // TODO: ルーム作成
     }
 }
 
