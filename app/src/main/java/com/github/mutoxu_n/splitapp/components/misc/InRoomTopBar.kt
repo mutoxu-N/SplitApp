befadb72ba43fe77128c7a35c5598e54950cfa3d
mutoxu=N <55544957+mutoxu-N@users.noreply.github.com>
2024-07-30 13:24:49 +0900

    InRoomTopBarをOutRoomTopBarを呼び出す形で実装した

--- app/src/main/java/com/github/mutoxu_n/splitapp/components/misc/InRoomTopBar.kt
+++ app/src/main/java/com/github/mutoxu_n/splitapp/components/misc/InRoomTopBar.kt
@@ -4,45 +4,21 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.github.mutoxu_n.splitapp.ui.theme.SplitAppTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun InRoomTopBar(
     title: String,
     onBackClicked: () -> Unit = {},
 ) {
-    TopAppBar(
-        modifier = Modifier
-            .shadow(elevation = 2.dp),
-        colors = TopAppBarDefaults.largeTopAppBarColors(
-            containerColor = MaterialTheme.colorScheme.primaryContainer,
-            titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
-        ),
-        title = { Text(text = title) },
-        navigationIcon = {
-            IconButton(onClick = { onBackClicked() }) {
-                Icon(
-                    imageVector = Icons.AutoMirrored.Default.ArrowBack,
-                    contentDescription = null,
-                )
-            }
-        },
+    OutRoomTopBar(
+        title = title,
+        onBackClicked = { onBackClicked() },
     )
 }
 
