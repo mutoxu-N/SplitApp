faebd43d74b53c32613992a86b4af4a90305f3d1
mutoxu=N <55544957+mutoxu-N@users.noreply.github.com>
2024-08-11 19:41:58 +0900

    BottomNavigationにプレビューを追加 BottomNavigationのレシートアイコンの変更 BottomNavigationのアイコンアニメーション

--- app/src/main/java/com/github/mutoxu_n/splitapp/components/misc/BottomNavigation.kt
+++ app/src/main/java/com/github/mutoxu_n/splitapp/components/misc/BottomNavigation.kt
@@ -1,31 +1,43 @@
 package com.github.mutoxu_n.splitapp.components.misc
 
 import androidx.annotation.StringRes
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ReceiptLong
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.outlined.Info
-import androidx.compose.material.icons.outlined.Receipt
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.github.mutoxu_n.splitapp.R
+import com.github.mutoxu_n.splitapp.activities.ui.theme.SplitAppTheme
 
 sealed class InRoomNavItem(
     val route: String,
     @StringRes val title: Int,
     val icon: ImageVector,
 ) {
-    data object Receipt: InRoomNavItem("receipt", R.string.bottom_nav_item_receipts, Icons.Outlined.Receipt)
+    data object Receipt: InRoomNavItem("receipt", R.string.bottom_nav_item_receipts, Icons.AutoMirrored.Filled.ReceiptLong)
     data object Info: InRoomNavItem("info", R.string.bottom_nav_item_info, Icons.Outlined.Info)
     data object Setting: InRoomNavItem("setting", R.string.bottom_nav_item_settings, Icons.Default.Settings)
 }
@@ -58,7 +70,10 @@ fun BottomNavigation(
                 },
                 icon = {
                     Column(
-                        horizontalAlignment = Alignment.CenterHorizontally
+                        Modifier
+                            .animateContentSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
                     ) {
                         Icon(
                             imageVector = item.icon,
@@ -67,11 +82,37 @@ fun BottomNavigation(
                         if(selected) {
                             Text(
                                 text = stringResource(id = item.title),
+                                textAlign = TextAlign.Center
                             )
                         }
                     }
                 }
             )
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun BottomNavigationPreview() {
+    SplitAppTheme {
+        val controller = rememberNavController()
+        Scaffold(
+            bottomBar = {
+                BottomNavigation(
+                    controller = controller
+                )
+            }
+        ) { innerPadding ->
+            NavHost(
+                modifier = Modifier.padding(innerPadding),
+                navController = controller,
+                startDestination = InRoomNavItem.Receipt.route,
+            ) {
+                composable(InRoomNavItem.Receipt.route) {}
+                composable(InRoomNavItem.Info.route) {}
+                composable(InRoomNavItem.Setting.route) {}
+            }
         }
     }
 }
