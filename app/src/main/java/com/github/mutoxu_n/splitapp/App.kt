9167758d3c61d60e008c63590de0398554b4e5d3
mutoxu=N <55544957+mutoxu-N@users.noreply.github.com>
2024-08-24 09:45:03 +0900

    ルームIDと表示名をステートに変更

--- app/src/main/java/com/github/mutoxu_n/splitapp/App.kt
+++ app/src/main/java/com/github/mutoxu_n/splitapp/App.kt
@@ -2,9 +2,12 @@ package com.github.mutoxu_n.splitapp
 
 import android.app.Application
 import android.content.Context
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import com.google.firebase.FirebaseApp
 
-class App: Application() {
+class App: Application(), AutoCloseable {
     companion object {
         private var _appContext: Context? = null
         val appContext: Context? get() = _appContext
@@ -12,59 +15,13 @@ class App: Application() {
         private const val SHARED_PREFERENCES_FILENAME = "SplitApp"
         // ルームIDを取得・設定
         private const val SHARED_PREFERENCES_KEY_ROOM_ID = "roomId"
-        var roomId: String?
-            get() {
-                val sharedPref =
-                    appContext?.getSharedPreferences(
-                        SHARED_PREFERENCES_FILENAME,
-                        MODE_PRIVATE
-                    ) ?: return null
-                return sharedPref.getString(SHARED_PREFERENCES_KEY_ROOM_ID, null)
-            }
-            set(value) {
-                val sharedPref =
-                    appContext?.getSharedPreferences(
-                        SHARED_PREFERENCES_FILENAME,
-                        MODE_PRIVATE
-                    ) ?: return
-                val editor = sharedPref.edit()
-
-                // 値の保存
-                if(value == null) {
-                    editor.remove(SHARED_PREFERENCES_KEY_ROOM_ID).apply()
-
-                } else {
-                    editor.putString(SHARED_PREFERENCES_KEY_ROOM_ID, value).apply()
-                }
-            }
+        var roomId: String? by mutableStateOf(null)
+            private set
 
         // 表示名を取得・設定
         private const val SHARED_PREFERENCES_KEY_DISPLAY_NAME = "displayName"
-        var displayName: String?
-            get() {
-                val sharedPref =
-                    appContext?.getSharedPreferences(
-                        SHARED_PREFERENCES_FILENAME,
-                        MODE_PRIVATE
-                    ) ?: return null
-                return sharedPref.getString(SHARED_PREFERENCES_KEY_DISPLAY_NAME, null)
-            }
-            set(value) {
-                val sharedPref =
-                    appContext?.getSharedPreferences(
-                        SHARED_PREFERENCES_FILENAME,
-                        MODE_PRIVATE
-                    ) ?: return
-                val editor = sharedPref.edit()
-
-                // 値の保存
-                if(value == null) {
-                    editor.remove(SHARED_PREFERENCES_KEY_DISPLAY_NAME).apply()
-
-                } else {
-                    editor.putString(SHARED_PREFERENCES_KEY_DISPLAY_NAME, value).apply()
-                }
-            }
+        var displayName: String?  by mutableStateOf(null)
+            private set
 
         // ルームIDのバリデーション
         fun validateRoomID(roomId: String): Boolean {
@@ -77,6 +34,34 @@ class App: Application() {
 
             return true
         }
+
+        suspend fun updateRoomId(roomId: String): Boolean {
+            if(validateRoomID(roomId)) {
+                this.roomId = roomId
+                return true
+            } else return false
+        }
+
+        suspend fun updateDisplayName(displayName: String) {
+            this.displayName = displayName
+        }
+    }
+
+    init {
+        val sharedPref =
+            appContext?.getSharedPreferences(
+                SHARED_PREFERENCES_FILENAME,
+                MODE_PRIVATE
+            )
+
+        if(sharedPref == null) {
+            roomId = null
+            displayName = null
+
+        } else {
+            roomId = sharedPref.getString(SHARED_PREFERENCES_KEY_ROOM_ID, null)
+            displayName = sharedPref.getString(SHARED_PREFERENCES_KEY_DISPLAY_NAME, null)
+        }
     }
 
     override fun onCreate() {
@@ -85,4 +70,19 @@ class App: Application() {
         FirebaseApp.initializeApp(applicationContext)
     }
 
+    override fun close() {
+        // 値の保存
+        val sharedPref =
+            appContext?.getSharedPreferences(
+                SHARED_PREFERENCES_FILENAME,
+                MODE_PRIVATE,
+            ) ?: return
+        val editor = sharedPref.edit()
+
+        if(roomId == null) editor.remove(SHARED_PREFERENCES_KEY_DISPLAY_NAME).apply()
+        else editor.putString(SHARED_PREFERENCES_KEY_DISPLAY_NAME, roomId).apply()
+
+        if(displayName == null) editor.remove(SHARED_PREFERENCES_KEY_DISPLAY_NAME).apply()
+        else editor.putString(SHARED_PREFERENCES_KEY_DISPLAY_NAME, displayName).apply()
+    }
 }
